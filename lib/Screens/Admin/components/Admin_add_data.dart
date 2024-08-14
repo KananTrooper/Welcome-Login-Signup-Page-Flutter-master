4297d1693990521fcd70c8107fae1c60321e3fc1
Chaiwut Techadamrongtham <Ctechadarngthrrm@gmail.com>
2024-08-14 21:18:07 +0700

    New

--- lib/Screens/Admin/components/Admin_add_data.dart
+++ lib/Screens/Admin/components/Admin_add_data.dart
@@ -12,11 +12,20 @@ class AdminAddData extends StatefulWidget {
 class _AdminAddDataState extends State<AdminAddData> {
   String? _selectedGender;
   DateTime? _selectedDate;
+  String? _selectedMaritalStatus;
+  String? _selectedEducationStatus;
+
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
   final TextEditingController _cityController = TextEditingController();
   final TextEditingController _districtController = TextEditingController();
   final TextEditingController _subDistrictController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _relativeAddressController =
+      TextEditingController();
+  final TextEditingController _medicalConditionController =
+      TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
 
   void _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -28,8 +37,19 @@ class _AdminAddDataState extends State<AdminAddData> {
     if (picked != null && picked != _selectedDate) {
       setState(() {
         _selectedDate = picked;
+        _calculateAge(picked);
       });
     }
+  }
+
+  void _calculateAge(DateTime birthDate) {
+    final DateTime today = DateTime.now();
+    int age = today.year - birthDate.year;
+    if (today.month < birthDate.month ||
+        (today.month == birthDate.month && today.day < birthDate.day)) {
+      age--;
+    }
+    _ageController.text = '$age';
   }
 
   @override
@@ -54,46 +74,12 @@ class _AdminAddDataState extends State<AdminAddData> {
                   ),
                 ),
                 SizedBox(height: 20),
-                Text(
-                  "ชื่อผู้ป่วย",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  ),
-                ),
-                SizedBox(height: 10),
                 _buildTextField(_nameController, "ชื่อผู้ป่วย"),
                 SizedBox(height: 20),
                 Row(
                   children: <Widget>[
                     Expanded(
-                      child: Row(
-                        children: <Widget>[
-                          Text("ชาย"),
-                          Radio<String>(
-                            value: 'ชาย',
-                            groupValue: _selectedGender,
-                            onChanged: (String? value) {
-                              setState(() {
-                                _selectedGender = value;
-                              });
-                            },
-                          ),
-                          Text("หญิง"),
-                          Radio<String>(
-                            value: 'หญิง',
-                            groupValue: _selectedGender,
-                            onChanged: (String? value) {
-                              setState(() {
-                                _selectedGender = value;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Expanded(
+                      flex: 4,
                       child: ElevatedButton(
                         onPressed: () => _selectDate(context),
                         child: Text(
@@ -104,23 +90,129 @@ class _AdminAddDataState extends State<AdminAddData> {
                         style: ElevatedButton.styleFrom(
                           foregroundColor: Colors.black,
                           backgroundColor: Colors.white,
-                          minimumSize:
-                              Size(double.infinity, 50), // ปรับให้เต็มความกว้าง
+                          minimumSize: Size(double.infinity, 50),
                           elevation: 2,
                         ),
+                      ),
+                    ),
+                    SizedBox(width: 10), // ช่องว่าง 10 พิกเซล
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("เพศ"),
+                          Row(
+                            children: <Widget>[
+                              Text("ชาย"),
+                              Radio<String>(
+                                value: 'ชาย',
+                                groupValue: _selectedGender,
+                                onChanged: (String? value) {
+                                  setState(() {
+                                    _selectedGender = value;
+                                  });
+                                },
+                              ),
+                              SizedBox(width: 10), // ช่องว่าง 10 พิกเซล
+                              Text("หญิง"),
+                              Radio<String>(
+                                value: 'หญิง',
+                                groupValue: _selectedGender,
+                                onChanged: (String? value) {
+                                  setState(() {
+                                    _selectedGender = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
                 SizedBox(height: 20),
-                Text(
-                  "ที่อยู่",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "สถานะสมรส",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text("โสด"),
+                              Radio<String>(
+                                value: 'โสด',
+                                groupValue: _selectedMaritalStatus,
+                                onChanged: (String? value) {
+                                  setState(() {
+                                    _selectedMaritalStatus = value;
+                                  });
+                                },
+                              ),
+                              SizedBox(width: 10), // ช่องว่าง 10 พิกเซล
+                              Text("สมรส"),
+                              Radio<String>(
+                                value: 'สมรส',
+                                groupValue: _selectedMaritalStatus,
+                                onChanged: (String? value) {
+                                  setState(() {
+                                    _selectedMaritalStatus = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "สถานะการศึกษา",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          DropdownButton<String>(
+                            value: _selectedEducationStatus,
+                            hint: Text("เลือกสถานะการศึกษา"),
+                            items: <String>[
+                              'ประถมศึกษา',
+                              'มัธยมศึกษา',
+                              'ปริญญาตรี',
+                              'ปริญญาโท',
+                              'ปริญญาเอก'
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                _selectedEducationStatus = newValue;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 20),
                 _buildTextField(_addressController, "ที่อยู่"),
                 SizedBox(height: 20),
                 Row(
@@ -138,10 +230,16 @@ class _AdminAddDataState extends State<AdminAddData> {
                     ),
                   ],
                 ),
+                SizedBox(height: 20),
+                _buildTextField(_phoneController, "เบอร์โทรศัพท์"),
+                SizedBox(height: 20),
+                _buildTextField(_relativeAddressController, "ที่อยู่ญาติ"),
+                SizedBox(height: 20),
+                _buildTextField(_medicalConditionController, "โรคประจำตัว"),
                 SizedBox(height: 50),
                 Center(
                   child: SizedBox(
-                    width: double.infinity, // ทำให้ปุ่มกดเต็มความกว้าง
+                    width: double.infinity,
                     child: ElevatedButton(
                       onPressed: () {
                         // Handle the form submission logic here
@@ -149,7 +247,7 @@ class _AdminAddDataState extends State<AdminAddData> {
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Color.fromARGB(255, 184, 66, 231),
                         elevation: 0,
-                        minimumSize: Size(double.infinity, 50), // ขนาดของปุ่ม
+                        minimumSize: Size(double.infinity, 50),
                       ),
                       child: Text(
                         "เพิ่มข้อมูล",
@@ -161,7 +259,7 @@ class _AdminAddDataState extends State<AdminAddData> {
                     ),
                   ),
                 ),
-                SizedBox(height: 20), // เพิ่มระยะห่างก่อนปุ่มย้อนกลับ
+                SizedBox(height: 20),
                 Center(
                   child: TextButton(
                     onPressed: () {
@@ -173,12 +271,11 @@ class _AdminAddDataState extends State<AdminAddData> {
                             return AdminWindow();
                           },
                         ),
-                      ); // ย้อนกลับไปหน้าก่อนหน้า
+                      );
                     },
                     child: const Text('ย้อนกลับ'),
                     style: TextButton.styleFrom(
-                      foregroundColor:
-                          Colors.blue, // เปลี่ยนสีข้อความปุ่มเป็นสีน้ำเงิน
+                      foregroundColor: Colors.blue,
                     ),
                   ),
                 ),
@@ -209,9 +306,12 @@ class _AdminAddDataState extends State<AdminAddData> {
           hintText: hintText,
           hintStyle: TextStyle(color: Colors.grey),
           border: InputBorder.none,
-          contentPadding: EdgeInsets.symmetric(
-              horizontal: 10, vertical: 15), // เพิ่ม padding
+          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
         ),
+        keyboardType:
+            hintText.contains("เบอร์โทรศัพท์") || hintText.contains("อายุ")
+                ? TextInputType.number
+                : TextInputType.text,
       ),
     );
   }
